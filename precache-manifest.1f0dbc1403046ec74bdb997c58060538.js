2dbc3969355c9c93387ffb86efa76680d444ff47
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-16 09:43:26 +0000

    Deploy camilleZJ/react-ui-components to github.com/camilleZJ/react-ui-components.git:gh-pages

---
+++ precache-manifest.1f0dbc1403046ec74bdb997c58060538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d67428be932d221a76926993cba3068",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.463535e0ac6d36f490c1.bundle.js"
+  },
+  {
+    "url": "runtime~main.463535e0ac6d36f490c1.bundle.js"
+  },
+  {
+    "revision": "7cec5abe11a087b3f5dd",
+    "url": "static/css/main.72107bf1.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.463535e0ac6d36f490c1.bundle.js"
+  },
+  {
+    "url": "vendors~main.463535e0ac6d36f490c1.bundle.js.LICENSE.txt"
+  }
+]);
